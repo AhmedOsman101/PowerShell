c891c62530cc64f4b5d1306f4c4bd1fdbf230af1
AhmadOthman <trollingosman101@outlook.com>
2024-02-14 22:11:17 +0200

    commit changes via PS script

--- getExtensions.ps1
+++ getExtensions.ps1
@@ -15,17 +15,24 @@ $jsonContent = @"
 
 # Check if the file already exists
 if (Test-Path -Path extensions.json) {
-  # Ask the user if they want to overwrite the existing file
-  $overwrite = Read-Host -Prompt 'The file extensions.json already exists. Do you want to overwrite it? (Y/N)'
-
-  # Check the user's response
-  if ($overwrite -eq 'Y' -or $overwrite -eq 'y') {
+  if ($args -contains '-o') {
     # Overwrite the file
     $jsonContent | Out-File -FilePath extensions.json
     Write-Host "The file extensions.json has been overwritten."
   }
-  else {
-    Write-Host "The existing file extensions.json was not overwritten."
+  elseif (-not $args -contains '-o') {
+    # Ask the user if they want to overwrite the existing file
+    $overwrite = Read-Host -Prompt 'The file extensions.json already exists. Do you want to overwrite it? (Y/N)'
+
+    # Check the user's response
+    if ($overwrite -eq 'Y' -or $overwrite -eq 'y') {
+      # Overwrite the file
+      $jsonContent | Out-File -FilePath extensions.json
+      Write-Host "extensions.json has been overwritten."
+    }
+    else {
+      Write-Host "original file was not overwritten."
+    }
   }
 }
 else {
